511964825cfd9682b4d4631fff3b4d6e8e0bda07
Luiz Torre <luizmptorre@gmail.com>
2021-01-27 18:49:40 -0300

    Terminando as entidades do Usuario

--- app/models/department_coordinator.rb
+++
@@ -1,3 +0,0 @@
-class DepartmentCoordinator < ApplicationRecord
-  belongs_to :user
-end
--- db/migrate/20210127213639_create_department_coordinators.rb
+++
@@ -1,9 +0,0 @@
-class CreateDepartmentCoordinators < ActiveRecord::Migration[5.2]
-  def change
-    create_table :department_coordinators do |t|
-      t.references :user, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
