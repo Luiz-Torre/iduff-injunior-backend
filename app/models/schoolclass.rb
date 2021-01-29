61930ec6a422dec6aa2e10d2a8e6a30ccd86eaef
Luiz Torre <luizmptorre@gmail.com>
2021-01-29 01:14:52 -0300

    Terminando as chamadas de Vagas

--- app/models/schoolclass.rb
+++ app/models/schoolclass.rb
@@ -2,7 +2,7 @@ class Schoolclass < ApplicationRecord
   belongs_to :subject
   belongs_to :teacher
  
-  validates :calendar, :classroom, :name, :subject_id,presence: true
+  validates :calendar, :vacancy,:classroom, :name, :subject_id,presence: true
   validates :teacher_id , presence: true, uniqueness: true
 
 end
