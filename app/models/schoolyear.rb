6d4a7f9db9597c2a0baa5444743b0054b02481ec
Luiz Torre <luizmptorre@gmail.com>
2021-01-29 19:57:15 -0300

    Validacao do Periodo Letivo

--- app/models/schoolyear.rb
+++ app/models/schoolyear.rb
@@ -1,6 +1,12 @@
 class Schoolyear < ApplicationRecord
 
-
+    validates :status, presence: :true
     enum status: {planning: 0,  registrations:1, progress:2, concluded: 3}
-
+    after_validation :check_year
+  
+    def check_year
+      if Schoolyear.find(1).present?
+        errors.add :already_exist, "Periodo existente, para mudanças atualize ele"
+      end
+    end
 end
