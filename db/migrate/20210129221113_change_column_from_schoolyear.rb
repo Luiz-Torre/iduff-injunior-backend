e80cb7033b970460c907af7cbc029d0efb4fe3db
Luiz Torre <luizmptorre@gmail.com>
2021-01-29 20:04:52 -0300

    Tentando resolver o problema do Heroku

--- db/migrate/20210129221113_change_column_from_schoolyear.rb
+++ db/migrate/20210129221113_change_column_from_schoolyear.rb
@@ -1,8 +1,8 @@
 class ChangeColumnFromSchoolyear < ActiveRecord::Migration[5.2]
   def change
-    change_column :schoolyears, :status, :integer, default: 0
-    change_column :schoolyears, :year, :integer, default: 0
-    change_column :schoolyears, :half, :integer, default: 1
+    change_column :schoolyears, :status, :integer
+    change_column :schoolyears, :year, :integer
+    change_column :schoolyears, :half, :integer
 
   end
 end
---
+++ db/migrate/20210129230410_change_columnfrom_schoolyears.rb
@@ -0,0 +1,13 @@
+class ChangeColumnfromSchoolyears < ActiveRecord::Migration[5.2]
+  def change
+    class ChangeColumnFromSchoolyear < ActiveRecord::Migration[5.2]
+      def change
+        change_column :schoolyears, :status, :integer, default: 0
+        change_column :schoolyears, :year, :integer, default: 0
+        change_column :schoolyears, :half, :integer, default: 1
+    
+      end
+    end
+    
+  end
+end
