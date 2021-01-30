c3f537c1e27fc38fa7a0982d143ec7d731451b91
Luiz Torre <luizmptorre@gmail.com>
2021-01-30 15:05:02 -0300

    Resolvendo o heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name,:street,:number,:district,:complement,:cep,:telephone,:cellphone,:nationality, :state, :RG, :birthdate, :email, :role, :cpf, :password, :password_confirmation)
+      params.require(:user).permit(:name,:street,:number,:district,:complement,:cep,:telephone,:registration,:cellphone,:nationality, :state, :RG, :birthdate, :email, :role, :cpf, :password, :password_confirmation)
     end
 end
